0a7480488b6e426421854a7f6cf9b53bd81d2854
Testacc991 <Testacc991@github.com>
2021-07-30 19:12:35 +0300

    little change

--- pershiy_naimenshiy.lua
+++ pershiy_naimenshiy.lua
@@ -1,17 +1,15 @@
 end0 = io.read()
 end1 = io.read()
 t = {}
+tsorted = {}
 for digit in string.gmatch(end1, "[-+]?%d+") do
   d = tonumber(digit)
   table.insert(t,d)
 end
-table.sort(t)
-sum = 0
-for i,v in ipairs(t) do
-  if v~=t[1] and v~=t[#t] then
-    sum=sum+v
-  end
-end
+tsorted = t
+table.sort(tsorted)
+persh = tsorted[1]
+for i,v in ipairs(t)
 --print(table.concat(t," "))
 print(sum)
 
